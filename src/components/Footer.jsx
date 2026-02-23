22355cc4613305f8217dfb46ac44bc645a180bad
SrRemolacha <kevinremolina10@gmail.com>
2026-02-23 17:46:43 -0500

    Initial project setup with base layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2026 Kevin Remolina</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Construyo experiencias web modernas</h1>
+    </section>
+  )
+}
+
+export default Hero
